c9373a2a65b44c7d5fce60dc610f3c8b3bb8579d
Paul <32818259+PaulDesprairies@users.noreply.github.com>
2019-02-07 14:49:52 +0100

    Un entrée / 4 lignes

---
+++ 1.4.3.py
@@ -0,0 +1,13 @@
+#print(len("06b483045022100ab44ef425e6d85c03cf301bc16465e3176b55bba9727706819eaf07cf84cf52d02203f7dc7ae9ab36bead14dd3c83c8c030bf8ce596e692021b66441b39b4b35e64e012102f63ae3eba460a8ed1be568b0c9a6c947abe9f079bcf861a7fdb2fd577ed48a81"))
+#print(len("Feffffff"))
+
+entree ="1941e985075825e09de53b08cdd346bb67075ef0ce5c94f98853292d4bf94c10d010000006b483045022100ab44ef425e6d85c03cf301bc16465e3176b55bba9727706819eaf07cf84cf52d02203f7dc7ae9ab36bead14dd3c83c8c030bf8ce596e692021b66441b39b4b35e64e012102f63ae3eba460a8ed1be568b0c9a6c947abe9f079bcf861a7fdb2fd577ed48a81Feffffff"
+subHash = entree[0:64]
+subIndex  =  entree[64:72]
+subScriptSig = entree[72:-8]
+subSequence  = entree[-8:]
+
+print("Hash :",subHash)
+print("Index :",subIndex)
+print("ScriptSid :",subScriptSig)
+print("Sequence :",subSequence)
