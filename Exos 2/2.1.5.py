a181a237e4e10aaa5b2610827f6e9594ed31190e
Paul <32818259+PaulDesprairies@users.noreply.github.com>
2019-02-12 16:56:18 +0100

    bloc reward

--- Exos 2/2.1.5.py
+++ Exos 2/2.1.5.py
@@ -1,8 +1,7 @@
 def blocReward(height):
     palier = int(height/210000)
-    reward = int((50 / (2**palier))) * 100000000)
-    print(palier)
-    return reward
+    reward = int((50 / (2**palier)) * 100000000)
+    return palier, reward
 
 height = 550000000000000
 print(blocReward(height))
