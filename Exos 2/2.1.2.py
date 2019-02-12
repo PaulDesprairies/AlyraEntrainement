9ff5f520e7b4f8c205544bb058844ceefefe2394
Paul <32818259+PaulDesprairies@users.noreply.github.com>
2019-02-12 15:44:18 +0100

    header --> difficulté

---
+++ Exos 2/2.1.2.py
@@ -0,0 +1,17 @@
+header = "0x1c0ae493"
+cible_max=((2**16)-1)*(2**208)
+
+zeroing = int(header[:4],16)
+cible = hex(int(header[4:],16))
+cible = str(cible)
+
+def extract_cible(data):
+    i = 0
+    while i < zeroing:
+        data = data + "0"
+        i = i+1
+    return data
+
+
+difficulte = cible_max/int(extract_cible(cible),16)
+print(difficulte)
