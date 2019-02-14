f7e5f612c1018cc170a42d62bc58ec96617298c1
Paul Desprairies <paul.desprairies@gmail.com>
2019-02-14 18:38:16 +0100

    3.1.1.2.sol

---
+++ exo3/3.1.1.2.sol
@@ -0,0 +1,20 @@
+//Write your own contracts here. Currently compiles using solc v0.4.15+commit.bbb8e64f.
+pragma solidity 0.4.25;
+contract Assemblee {
+  address[] membres; 
+
+function rejoindre() public{
+  membres.push(msg.sender);
+}
+
+function estmembre(address utilisateur) public view returns (bool){
+ for (uint i=0; i<membres.length; i++) {
+  if (msg.sender == membres[i]){
+     return true;
+  i += 1;
+   }
+  return false
+ }
+
+}
+}
