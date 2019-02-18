d1a19e56bad0c8ede4e0fd4880577c876f43c6dd
Paul Desprairies <paul.desprairies@gmail.com>
2019-02-18 15:03:48 +0100

    3.1.3.1

--- exo3/3.1.3.1.sol
+++ exo3/3.1.3.1.sol
@@ -2,72 +2,133 @@
 
 pragma solidity 0.4.25;
 contract Assemblee {
-  address[] membres; 
+  
+  string public nomAssemble;
 
-struct Decision{
-  string[] descriptionDecisions;
-  uint[] voteContre;
-  uint[] votePour;
-  uint[] timeStamp;
-  mapping( address => bool) aVote;
+
+  struct Habilitation{
+    address membres;
+    mapping( address => bool) estAdmin;
+    uint8 blame;
+  } 
+
+  struct Decision{
+    string descriptionDecisions;
+    uint voteContre;
+    uint votePour;
+    uint timeStamp;
+    mapping( address => bool) aVote;
+  }
+
+Habilitation[] habilitations;
+Decision[] decisions;
+
+
+//initialisation du contrat, premier admin, nommage de l'assemblée
+constructor(string nom) public {
+    nomAssemble = nom;
+    habilitations[0].membres = msg.sender;
+    habilitations[0].estAdmin[msg.sender] = true;
+    habilitations[0].blame = 0;
 }
 
 
-
-Decision[4] decisions;
-
-function decisionLenght() public constant returns(uint) {
-    return decisions.length;
+//rejoindre l'assemblée
+function rejoindre() public {
+  require((estMembre(msg.sender) >= 0), "Vous êtes déjà membre !");
+  Habilitation memory welcome;
+  welcome.membres = msg.sender;
+  welcome.blame = 0;
+  habilitations.push(welcome);
 }
 
-function rejoindre() public{
-  membres.push(msg.sender);
-}
 
-function estMembre(address utilisateur) public view returns (bool){
- for (uint i=0; i<membres.length; i++) {
-  if (msg.sender == membres[i]){
-     return true;
-  i += 1;
+//Vérifie si une personne est déjà membre ou non et renvoit un index si la décision existe
+function estMembre(address utilisateur) public view returns (uint) {
+ for (uint i=0; i < habilitations.length-1; i++) {
+  if (msg.sender == habilitations[i].membres){
+     return i;
    }
-  return false;
- }
-}
-
-function proposerDecision(string description) public{
-  if (estMembre(msg.sender)){
-    decisions[0].descriptionDecisions.push(description);
-    decisions[1].votePour.push(0);
-    decisions[2].voteContre.push(0);
-    decisions[3].timeStamp.push(now);
+  i += 1;
   }
 }
 
-function voter(string description, uint vote) public view returns (bool){
-  require(estMembre(msg.sender), "Il faut être membre !");
-    uint8 dec = 0;
-    for (uint i=0; i < decisionLenght(); i++){
-      if (keccak256(abi.encode(decisions[i].descriptionDecisions)) == keccak256(abi.encode(decisions[i].descriptionDecisions[i]))){
-        require(now - decisions[i].timeStamp[i] < 604800 ); //erreur ici
-        require((decisions[i].aVote[msg.sender]==false), "Vous avez déjà voté !");
-        require(vote == 1 || vote == 0, "Il faut voter 1 pour oui ou 0 pour non.");
-        if (vote == 1){
-          decisions[i].votePour[i] +=1;
-        }else if (vote == 0){
-          decisions[i].voteContre[i] +=1;
-        }
+
+//Soumettre une décision
+function proposerDecision(string description) public {
+  require(estMembre(msg.sender) >= 0, "Il faut être membre !");
+  Decision memory nouvelleDecision;
+    nouvelleDecision.descriptionDecisions = description;
+    nouvelleDecision.votePour = 0;
+    nouvelleDecision.voteContre = 0;
+    nouvelleDecision.timeStamp = now;
+    decisions.push(nouvelleDecision);
+  }
+
+
+//Vérifie si la décision est déjà soumise ou non et renvoit un index si la décision existe
+function decisionRedondante(string decision) public view returns (uint) {
+ for (uint i=0; i < decisions.length-1; i++) {
+  if (keccak256(decision) == keccak256(decisions[i].descriptionDecisions)) {
+     return i;
+   }
+  i += 1;
+  }
+}
+
+//Voter une décision sur un délai de 7 jours en comptabilisant les résultats à chaque vote
+function voter(string description, uint vote) public view returns (bool) {
+    require(estMembre(msg.sender) >= 0, "Il faut être membre !");
+    require(decisionRedondante(description) >= 0, "Vous avez déjà proposé cette décision !");
+    require(now - decisions[i].timeStamp < 604800, "Vous votez trop tard sur cette décision" );
+    require((decisions[i].aVote[msg.sender]==false), "Vous avez déjà voté !");
+    require(vote == 1 || vote == 0, "Il faut voter 1 pour oui ou 0 pour non.");
+    uint i = decisionRedondante(description);
+
+      if (vote == 1){
+        decisions[i].votePour += 1;
+      }else if (vote == 0){
+        decisions[i].voteContre += 1;
+      }
 
       decisions[i].aVote[msg.sender] = true;
-      dec = 1;
+      comptabiliser(i);
     }
-    if (dec == 0) {
-      revert("Il faut d'abord soumettre une décision !");
-    } 
 
-  }
+
+//Comptabilisation des votes
+function comptabiliser(uint indice) public view returns (int) {
+  return int(decisions[indice].votePour)-int(decisions[indice].voteContre);
 }
 
-function comptabiliser(uint indice) public view returns (int){
-  return int(decisions[indice].votePour[indice])-int(decisions[indice].voteContre[indice]);
+// Nommer un administrateur
+function nommerAdmin(address promu) public view {
+  require(habilitations[estMembre(promu)].estAdmin[msg.sender], "Il faut être administrateur pour effectuer cette action.");
+  require(estMembre(promu) >= 0);
+  habilitations[estMembre(promu)].estAdmin[promu] = true;
+}
+
+//Démission d'un administrateur
+function demission() public view {
+  require(habilitations[estMembre(msg.sender)].estAdmin[msg.sender], "Il faut être administrateur pour effectuer cette action.");
+  habilitations[estMembre(msg.sender)].estAdmin[msg.sender] = false;
+}
+
+//Fermer proposition de décision
+function veto(string description) public view {
+  require(habilitations[estMembre(msg.sender)].estAdmin[msg.sender], "Il faut être administrateur pour effectuer cette action.");
+  delete decisions[decisionRedondante(description)];
+}
+
+//Attribuer un blame et expulser au second blame
+function blamer(address puni) public view {
+  require(habilitations[estMembre(msg.sender)].estAdmin[msg.sender], "Il faut être administrateur pour effectuer cette action.");
+  require(estMembre(puni)>=0, "Vous ne pouvez attribuer un blâme à un non-membre.");
+  habilitations[estMembre(puni)].blame =+ 1;
+
+  if (habilitations[estMembre(puni)].blame == 2){
+    delete habilitations[estMembre(puni)];
+  }
+
 }
 }
