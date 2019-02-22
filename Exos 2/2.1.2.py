9a07657d7b23aeb6eb562fbfb2e8c44c43d73fe0
Paul Desprairies <paul.desprairies@gmail.com>
2019-02-22 12:31:39 +0100

    maj

--- Exos 2/2.1.2.py
+++ Exos 2/2.1.2.py
@@ -1,16 +1,16 @@
 header = "0x1c0ae493"
 cible_max=((2**16)-1)*(2**208)
 
-zeroing = int(header[:4],16)
-cible = hex(int(header[4:],16))
-cible = str(cible)
+exp = int(header[:4],16)
+coeff = hex(int(header[4:],16))
+coeff = str(coeff)
 
 def extract_cible(data):
     i = 0
-    while i < 2 * zeroing - 6:
+    while i < 2 * exp - 6:
         data = data + "0"
         i = i+1
     return data
 
-difficulte = cible_max/int(extract_cible(cible),16)
-print(difficulte)
+difficulte = cible_max/int(extract_cible(coeff),16)
+print(difficulte, "&", int(extract_cible(coeff),16))
