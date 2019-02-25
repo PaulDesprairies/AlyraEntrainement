b780098f121241dacc600b71e94ec48a9e6609fc
Paul Desprairies <paul.desprairies@gmail.com>
2019-02-25 10:55:35 +0100

    almost there

---
+++ test.py
@@ -0,0 +1,8 @@
+number = 2.5
+print(number, 'in hex =', float.hex(number))
+
+number = 0.0
+print(number, 'in hex =', float.hex(number))
+
+number = 10.5
+print(number, 'in hex =', float.hex(number))
