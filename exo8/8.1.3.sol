f7aa38445e006fee718a04ad0c43453febadadb8
pauldesprairies <paul.desprairies@gmail.com>
2019-04-08 15:28:06 +0200

    fix 8.1.3

--- exo8/8.1.3.sol
+++ exo8/8.1.3.sol
@@ -12,35 +12,33 @@ contract CanalDePaiement{
     uint equilibreA;
     uint equilibreB;
 
-    constructor(uint _montant, address _partieA, address _partieB) internal{
+    constructor(uint _montant, address _partieA, address _partieB) public{
         montant = _montant;
         partieA = _partieA;
         partieB = _partieB;
         etat = EtatCanal.VIDE;
     }
     
-    function financer() payable public{
+    function financer() payable public returns (EtatCanal){
         require(msg.value == montant, "Montant incorrect");
-        bool A;
-        bool B;
-
-        if(msg.sender == partieA){
-            A = true;
+        if(msg.sender == partieA ){
+            require(equilibreA == 0,"Montant déjà versé");
             equilibreA += montant;
         }
-
-        if(msg.sender == partieB){
-            B = true;
+        if(msg.sender == partieB ){
+            require(equilibreB == 0,"Montant déjà versé");
             equilibreB += montant;
         }
-
-        if(A == true && B == true){
+        if(equilibreA == montant && equilibreB == montant){
             etat = EtatCanal.ACTIF;
         }
+        
+        return etat;
     }
+
     
 
-    function message(uint virement) public returns (bytes32){
+    function virer(uint virement) public returns (bytes32){
 
         if (msg.sender == partieA){
             require(equilibreA >= virement, "Vous n'avez pas les fonds suffisants");
@@ -55,7 +53,11 @@ contract CanalDePaiement{
             equilibreB -= virement;
             dernierNonce += 1;
         }
-        return keccak256(abi.encodePacked(dernierNonce, equilibreA, equilibreB));
+        return message(dernierNonce, equilibreA, equilibreB);
+    }
+
+    function message(uint a, uint b, uint c) pure public returns(bytes32){
+      return keccak256(abi.encodePacked(a,b,c)); 
     }
 
 }
